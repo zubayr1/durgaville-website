5650f2ad0768a1cf95ce9add24da8edc9379fa52
Zubayr Khalid <zkhalid@audeering.com>
2024-04-19 12:38:31 +0200

    fix date style

--- src/components/Events.js
+++ src/components/Events.js
@@ -41,6 +41,35 @@ function Events() {
     fetchData();
   }, []);
 
+
+  function formatDate(dateString) {
+    const date = new Date(dateString);
+    const options = { day: 'numeric', month: 'short', year: 'numeric' };
+    const formattedDate = new Intl.DateTimeFormat('en-US', options).format(date);
+    
+    // Extract day, month, and year
+    const month = formattedDate.split(' ')[0];
+    
+    // Add suffix to the day
+    const dayWithSuffix = addDaySuffix(date.getDate());
+
+    return `${dayWithSuffix} ${month}, ${date.getFullYear()}`;
+  }
+
+  // Function to add suffix to the day (e.g., 1st, 2nd, 3rd, 4th, ...)
+  function addDaySuffix(day) {
+    console.log(day);
+    if (day === 1 || day === 21 || day === 31) {
+        return day + "st";
+    } else if (day === 2 || day === 22) {
+        return day + "nd";
+    } else if (day === 3 || day === 23) {
+        return day + "rd";
+    } else {
+        return day + "th";
+    }
+  }
+
   return (
     <div style={{ backgroundColor:"#dee0e3" }}>
       <div className="grid-container-events margin-container">
@@ -48,6 +77,7 @@ function Events() {
           <div className="grid-item-events" key={item.id} onClick={() => handleItemClick(item)}> 
             <div className="item-content-events">
               <h4 className="item-title-events">{item.title}</h4>
+              <p className="item-date-events">{formatDate(item.date)}</p>
               <div className="content-container">
                 <img src={item.imageUrl} alt={item.title} className="item-image-events" />
                 <p className="item-description-events">{item.description}</p>
