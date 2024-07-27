83ab104a3891e9ec8944e5edc486e74af7b94538
Khabbab Zakaria <zakariak.engg@gmail.com>
2024-07-27 18:23:36 +0200

    video, sliding in Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,45 @@
-import React from 'react'
-import { Divider, Grid, Image } from 'semantic-ui-react'
-import thakurhome from '../assets/thakurHome.jpg'
+import React from 'react';
+import { Divider, Grid, Image } from 'semantic-ui-react';
+import Slider from 'react-slick';
+import thakurhome from '../assets/thakurHome.jpg';
+import mag from '../assets/magazine_entry.jpg';
 
-import './home.css'
-import MagazineDeclare from './MagazineDeclare'
+import './home.css';
+import MagazineDeclare from './MagazineDeclare';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 function Home() {
+  const sliderSettings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    centerMode: true,
+    centerPadding: '0',
+    autoplay: true,
+    autoplaySpeed: 3000,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+
   return (
     <div style={{ backgroundColor: '#dee0e3' }}>
       <div className="margin-container">
@@ -35,6 +69,19 @@ function Home() {
             <div style={{ display: 'flex', justifyContent: 'center' }}>
               <Image alt="Thakur Durgaville" src={thakurhome} style={{ height: '96vh' }} />
             </div>
+
+            {/* Embed Video */}
+            <div className="video-container">
+              <iframe
+                width="100%"
+                height="auto"
+                src="https://www.youtube.com/embed/tofLwpMQ0LI?si=uLsQfkEp-WofoZFm"
+                title="YouTube video player"
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+              ></iframe>
+            </div>
           </Grid.Column>
 
           <Grid.Column only="tablet" width={16} textAlign="middle">
@@ -61,6 +108,19 @@ function Home() {
 
             <div style={{ display: 'flex', justifyContent: 'center', padding: '6%' }}>
               <Image alt="Thakur Durgaville" src={thakurhome} style={{ height: '80vh' }} />
+            </div>
+
+            {/* Embed Video */}
+            <div className="video-container">
+              <iframe
+                width="100%"
+                height="auto"
+                src="https://www.youtube.com/embed/tofLwpMQ0LI?si=uLsQfkEp-WofoZFm"
+                title="YouTube video player"
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+              ></iframe>
             </div>
           </Grid.Column>
 
@@ -89,17 +149,47 @@ function Home() {
             <div style={{ display: 'flex', justifyContent: 'center', padding: '6%' }}>
               <Image alt="Thakur Durgaville" src={thakurhome} />
             </div>
+
+            {/* Embed Video */}
+            <div className="video-container">
+              <iframe
+                width="100%"
+                height="auto"
+                src="https://www.youtube.com/embed/tofLwpMQ0LI?si=uLsQfkEp-WofoZFm"
+                title="YouTube video player"
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+              ></iframe>
+            </div>
           </Grid.Column>
         </Grid>
 
         <Divider />
 
         <Grid centered>
-          <MagazineDeclare/>
+          <MagazineDeclare />
         </Grid>
+
+        <div style={{ marginTop: '5%', maxWidth: '80%', margin: '0 auto' }}>
+          <Slider {...sliderSettings}>
+            <div className="slider-item">
+              <img src={thakurhome} alt="Thakur Durgaville" className="slider-image" />
+            </div>
+            <div className="slider-item">
+              <img src={mag} alt="Mag Durgaville" className="slider-image" />
+            </div>
+            <div className="slider-item">
+              <img src={thakurhome} alt="Thakur Durgaville" className="slider-image" />
+            </div>
+            <div className="slider-item">
+              <img src={mag} alt="Mag Durgaville" className="slider-image" />
+            </div>
+          </Slider>
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
